d30382cd0f5e1f48c345e9295eb428d8045edde6
Sasha <Oleksbielianinov@gmail.com>
2016-06-29 17:22:24 +0300

    Start done

---
+++ js/_assets-sprockets.js
@@ -0,0 +1,6 @@
+//@prepros-append ../bower_components/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js
+//@prepros-append ../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
